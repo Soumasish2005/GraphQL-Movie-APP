7d31c5855442e5aafd00df4077cb3d0782e3f21b
Soumasish Dasgupta <dasguptasoumasish@gmail.com>
2025-03-16 17:35:43 +0530

    feat: modified graphQL typeDefs

--- ai-movie-app/server/schema/typeDefs.js
+++ ai-movie-app/server/schema/typeDefs.js
@@ -24,17 +24,7 @@ export const typeDefs = gql`
         id: ID!
         name: String!
         isInTheatres: Boolean
-        Director: String
-        releaseYear: Int
-        genre: String
-        rating: Float
-        studio: String
-        runtime: Int
-    }
-
-    input AnimeFilterInput {
-        name: String
-        Director: String
+        director: String
         releaseYear: Int
         genre: String
         rating: Float
@@ -45,12 +35,39 @@ export const typeDefs = gql`
         getAllMovies: [Movie!]!
         getAllUsers: [User!]!
         getAllAnimeMovies: [AnimeMovie!]!
-        filterMoviesByRating(rating: Float): [Movie]
-        filterAnimesByInput(filter: AnimeFilterInput): [AnimeMovie]
+        getUserById(id: ID!): User
+        getMovieById(id: ID!): Movie
+        getAnimeMovieById(id: ID!): AnimeMovie
+        filterMoviesByInput(filter: MovieInput): [Movie]
+        filterAnimesByInput(filter: AnimeMovieInput): [AnimeMovie]
+    }
+    input MovieInput {
+        name: String!
+        isInTheatres: Boolean
+        director: String
+        releaseYear: Int
+        genre: String
+        rating: Float
+    }
+    input AnimeMovieInput {
+        name: String
+        isInTheatres: Boolean
+        director: String
+        releaseYear: Int
+        genre: String
+        rating: Float
+        studio: String
+        runtime: Int
     }
     type Mutation {
         createUser(name: String!, email: String!, password: String!): User
         updateUser(id: ID!, name: String, email: String, password: String): User
         deleteUser(id: ID!): User
+        addMovie(input: MovieInput): Movie
+        addAnimeMovie(input: AnimeMovieInput): AnimeMovie
+        updateMovie(input: MovieInput): Movie
+        updateAnimeMovie(input: AnimeMovieInput): AnimeMovie
+        deleteMovie(id: ID!): Movie
+        deleteAnimeMovie(id: ID!): AnimeMovie
     }
 `;
